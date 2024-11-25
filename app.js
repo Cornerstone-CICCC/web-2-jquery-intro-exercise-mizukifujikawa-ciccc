37ea9671fba8511428b742f234186c6b7ea7ba63
mizukifujikawa-ciccc <mz.fujikawa.ciccc@gmail.com>
2024-11-25 11:45:43 -0800

    exercise

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 $(function() {
   // YOUR CODE HERE
+  $('#toggleButton').on('click', () => {
+    $('#myParagraph').toggle();
+  });
+
+  $('#colorButton').on('click', () => {
+    $('#colorDiv').css({
+      'background-color': 'yellow'
+    });
+  });
+
+  $('#addClassButton').on('click', () => {
+    $('#classDiv').addClass('class');
+  });
+  $('#removeClassButton').on('click', () => {
+    $('#classDiv').removeClass('class');
+  });
+
+  $('#fadeInButton').on('click', () => {
+    $('#fadeDiv').fadeIn();
+  });
+  $('#fadeOutButton').on('click', () => {
+    $('#fadeDiv').fadeOut();
+  });
+
+  $('#slideUpButton').on('click', () => {
+    $('#slideDiv').slideUp();
+  });
+  $('#slideDownButton').on('click', () => {
+    $('#slideDiv').slideDown();
+  });
 })
